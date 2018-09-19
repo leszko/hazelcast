a81a985430f36e22d12ff94975255bec12e47649
Rafal Leszko <rafal@hazelcast.com>
2018-09-19 17:43:44 +0200

    Add DiscoveryAliasMapperTest

--- hazelcast/src/main/java/com/hazelcast/config/DiscoveryAliasMapper.java
+++ hazelcast/src/main/java/com/hazelcast/config/DiscoveryAliasMapper.java
@@ -1,18 +1,17 @@
 /*
+ * Copyright (c) 2008-2018, Hazelcast, Inc. All Rights Reserved.
  *
- *  * Copyright (c) 2008-2018, Hazelcast, Inc. All Rights Reserved.
- *  *
- *  * Licensed under the Apache License, Version 2.0 (the "License");
- *  * you may not use this file except in compliance with the License.
- *  * You may obtain a copy of the License at
- *  *
- *  * http://www.apache.org/licenses/LICENSE-2.0
- *  *
- *  * Unless required by applicable law or agreed to in writing, software
- *  * distributed under the License is distributed on an "AS IS" BASIS,
- *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  * See the License for the specific language governing permissions and
- *  * limitations under the License.
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  ee the License for the specific language governing permissions and
+ *  limitations under the License.
  *
  */
 
@@ -52,6 +51,8 @@ public class DiscoveryAliasMapper {
     }
 
     private static DiscoveryStrategyConfig createDiscoveryStrategyConfig(AliasedDiscoveryConfig config) {
+        validateConfig(config);
+
         String className = ALIAS_MAPPINGS.get(config.getEnvironment());
         Map<String, Comparable> properties = new HashMap<String, Comparable>();
         for (String key : config.getProperties().keySet()) {
@@ -60,8 +61,14 @@ public class DiscoveryAliasMapper {
         return new DiscoveryStrategyConfig(className, properties);
     }
 
+    private static void validateConfig(AliasedDiscoveryConfig config) {
+        if (!DISCOVERY_ALIASES.contains(config.getEnvironment())) {
+            throw new InvalidConfigurationException(String.format("Unknown environment tag: '%s'", config.getEnvironment()));
+        }
+    }
+
     private static void putIfNotNull(Map<String, Comparable> properties, String key, String value) {
-        if (key != null && value != null) {
+        if (key != null) {
             properties.put(key, value);
         }
     }
